b872beffcf1f15c226e0ed0a60b29812f15dc71b
AntoniX35 <62710828+AntoniX35@users.noreply.github.com>
2020-09-06 22:08:06 +0300

    Add files via upload

---
+++ config.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "f4se_common/f4se_version.h"
+
+//-----------------------
+// Plugin Information
+//-----------------------
+#define PLUGIN_VERSION              14
+#define PLUGIN_VERSION_STRING       "0.4.0"
+#define PLUGIN_NAME_SHORT           "HighFPSPhysicsFix"
+#define PLUGIN_NAME_LONG            "HighFPSPhysicsFix"
+#define SUPPORTED_RUNTIME_VERSION   CURRENT_RELEASE_RUNTIME
+#define MINIMUM_RUNTIME_VERSION     RUNTIME_VERSION_1_10_163
+#define COMPATIBLE(runtimeVersion)  (runtimeVersion == SUPPORTED_RUNTIME_VERSION)
